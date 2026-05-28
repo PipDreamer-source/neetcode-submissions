1e2faea0a161cd6b607d4c2d7f73cfc6fd83060b
PipDreamer <47395708+PipDreamer-source@users.noreply.github.com>
2026-05-28 20:48:59 +0530

    Add: best-time-to-buy-and-sell-stock-ii - submission-1

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+
+
+      // always remember greedy technique is best 
+
+         int maxProfit=0;
+        for(int i=1;i<prices.length;i++){
+
+
+               if(prices[i] > prices[i-1]){
+
+                maxProfit += (prices[i] - prices[i-1]);
+               }
+        }
+        return maxProfit;
+        
+    }
+}
